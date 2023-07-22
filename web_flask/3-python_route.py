3567160c1c48f3515f00fef599e7579654f2ed71
Delight Gbolahan <olagbujidelight@gmail.com>
2023-07-22 01:08:12 +0000

    Add new Python route

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -3,7 +3,10 @@
 
 The application listens on 0.0.0.0, port 5000.
 Routes:
-    /: Displays 'Hello HBNB!'
+    /: Displays 'Hello HBNB!'.
+    /hbnb: Displays 'HBNB'.
+    /c/<text>: Displays 'C' followed by the value of <text>.
+    /python/(<text>): Displays 'Python' followed by the value of <text>.
 """
 from flask import Flask
 
@@ -12,28 +15,36 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
-	"""Displays 'Hello HBNB!'"""
-	return "Hello HBNB!"
+    """Displays 'Hello HBNB!'."""
+    return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-	"""Displays 'HBNB'"""
-	return "HBNB"
+    """Displays 'HBNB'."""
+    return "HBNB"
+
 
 @app.route("/c/<text>", strict_slashes=False)
-def hbnb_c(text):
-	"""display 'C' followed by the value of the text variable"""
-	new_text = text.replace("_", " ")
-	return "C {}".format(new_text)
+def c(text):
+    """Displays 'C' followed by the value of <text>.
+
+    Replaces any underscores in <text> with slashes.
+    """
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
 
 @app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python(text="is cool"):
-	"""display 'C' followed by the value of the text variable"""
-	new_text = text.replace("_", " ")
-	return "Python {}".format(new_text)
+    """Displays 'Python' followed by the value of <text>.
+
+    Replaces any underscores in <text> with slashes.
+    """
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
 
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0')
+    app.run(host="0.0.0.0")
